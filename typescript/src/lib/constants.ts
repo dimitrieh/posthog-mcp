adc55f9bfefa237d5c0cd7541fb6b5bc24c71960
Dimitrie Hoekstra <hi@dimitr.ie>
2025-08-06 17:27:30 +0200

    Update to eu server

--- typescript/src/lib/constants.ts
+++ typescript/src/lib/constants.ts
@@ -3,4 +3,4 @@ import { env } from "cloudflare:workers";
 export const DEV = false;
 
 export const BASE_URL =
-	env.POSTHOG_BASE_URL || (DEV ? "http://localhost:8010" : "https://us.posthog.com");
+	env.POSTHOG_BASE_URL || (DEV ? "http://localhost:8010" : "https://eu.posthog.com");
